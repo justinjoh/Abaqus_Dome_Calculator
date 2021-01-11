d97c1e46a99a0ccf3c6dd092b5e000c324cef8d8
justinohpaper <justin.oh@paperlessparts.com>
2021-01-10 21:27:10 -0500

    cleanup config.py

--- config.py
+++ config.py
@@ -2,16 +2,18 @@
 
 from decimal import Decimal
 
-#------------------------------------------------------------------
+# ------------------------------------------------------------------
 # User defined variables
 
-R_mm = 2 # Radius of the circle in meters or millimeters. Either will work as long as the rest of your simulation follows the same units
-frequency_n = 2     # Frequency of the geodesic
-Dome_calc = True    # Calculate for sphere (=False) or dome (=True)
-Icosohedral = False #Set to false to generate spherical dome or true for icosohedral dome
+# Radius of the circle in meters or millimeters.
+# Either will work as long as the rest of your simulation follows the same units
+R_mm = 2
+frequency_n = 2  # Frequency of the geodesic
+Dome_calc = True  # Calculate for sphere (=False) or dome (=True)
+Icosohedral = False  # Set to false to generate spherical dome or true for icosohedral dome
 Cylindrical = False
-Cut_Point = .8 #Set to a number 0<x<1 to determine point at which the cut is made
-#------------------------------------------------------------------
+Cut_Point = .8  # Set to a number 0<x<1 to determine point at which the cut is made
+# ------------------------------------------------------------------
 # System variablesrivedr
 
 nPoint = 1
